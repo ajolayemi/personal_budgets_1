bdcbea05d68ac9a60e0dd194681b8f00db6352fc
jolayemi_local <ajolayemi82@gmail.com>
2022-08-31 11:49:06 +0200

    required apiRouter and mounted it at /pb path

--- assets/js/server.js
+++ assets/js/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const { apiRouter } = require('./api');
 
 const app = express();
+module.exports = app;
+
 const PORT = process.env.PORT || 3000;
+
+// Monting apiRouter at /pb path which will serve as my base path
+app.use('/pb', apiRouter)
+
 
 app.get('/', (req, res) => {
     res.send('Hello, World');
