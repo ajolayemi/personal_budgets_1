50366c7747b3be7b9dc0ad7edd5186ff553e7623
jolayemi_local <ajolayemi82@gmail.com>
2022-08-31 12:52:19 +0200

    defined verifyEnv and addEnvelopes funcs

--- assets/js/db.js
+++ assets/js/db.js
@@ -1,16 +1,35 @@
-const envelopes = [{nextId: 0}];
+const envelopes =  {
+    data: [],
+    nextId: 0,
+}
 
 
 // Verify envope body
 const verifyEnv = toVerify => {
-    const checkProps = toVerify.hasOwnProperty('title') && toVerify.hasOwnProperty('budget');
-    console.log(checkProps);
-    return checkProps;
+    if (!toVerify.hasOwnProperty('title') || toVerify.title === "") {
+        throw new Error('Title must be provided for envelope');
+    };
+
+    if (!toVerify.hasOwnProperty('budget') || toVerify.budget === "") {
+        throw new Error('Budget must be provided for envelope');
+    }
+    
+    if (isNaN(Number(toVerify.budget))) {
+        throw new Error('Budget must be a number');
+    };
+
+    return true; 
 }
 
 // Creates new envelopes
 const addEnvelopes = dataToAdd => {
-    const check = verifyEnv(dataToAdd);
+    if (verifyEnv(dataToAdd)) {
+        dataToAdd['id'] = envelopes.nextId += 1;
+        envelopes.data.push(dataToAdd);
+        return envelopes.data[envelopes.data.length - 1]
+    } else {
+        return null;
+    }
 } 
 
 module.exports = {
