589ee6e021b4a2689f913baa0b13b3d2414d72c8
jolayemi_local <ajolayemi82@gmail.com>
2022-08-31 12:51:55 +0200

    defined post route for pb/envelopes

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const { addEnvelopes } = require('./db');
 const app = require('./server')
 
 const apiRouter = express.Router();
 
+
+apiRouter.post('/envelopes', (req, res, next) => {
+    try {
+        const added = addEnvelopes(req.body);
+        res.send(added)
+    } catch (e) {
+        next(e)
+    }
+})
 
 module.exports = {
     apiRouter
