58048d03aeacdf25744eb136249607a69a9efeb0
jolayemi_local <ajolayemi82@gmail.com>
2022-08-31 14:44:15 +0200

    Defined get method for pb/envelopes/:envelopeId

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,10 +1,22 @@
 const express = require('express');
-const { addEnvelopes, getAllEnv } = require('./db');
+const { addEnvelopes, getAllEnv, getEnvById } = require('./db');
 const app = require('./server')
 
 const apiRouter = express.Router();
 const envelopeRouter = express.Router({mergeParams: true});
 apiRouter.use('/envelopes', envelopeRouter);
+
+envelopeRouter.param('envelopeId', (req, res, next, id) => {
+    const providedId = Number(id);
+    if (!isNaN(providedId)) {
+        req.envId = providedId;
+        next();
+    } else {
+        const invalidArgError = new Error('Provided envelope id must be a number');
+        invalidArgError.status = 400;
+        next(invalidArgError);
+    }
+})
 
 
 envelopeRouter.get('/', (req, res) => {
@@ -17,6 +29,17 @@ envelopeRouter.post('/', (req, res, next) => {
         res.send(added)
     } catch (e) {
         next(e)
+    }
+})
+
+envelopeRouter.get('/:envelopeId', (req, res, next) => {
+    const result = getEnvById(req.envId);
+    if (result) {
+        res.send(result)
+    } else {
+        const idError = new Error(`ID: ${req.envId} not found in database`);
+        idError.status = 404;
+        next(idError)
     }
 })
 
